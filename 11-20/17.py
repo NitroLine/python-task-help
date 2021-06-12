3c1bf21756e91ef4eb3f6bc436455b156194ae64
danilkaz <cmaster057@gmail.com>
2021-06-12 17:07:27 +0500

    17.py

---
+++ 11-20/17.py
@@ -0,0 +1,13 @@
+# Написать кэширующий декоратор, для функции f, если f(x) и f(x=) можно считать разными вызовами(40 баллов).
+
+
+def cache(f):
+    values = dict()
+
+    def wrapper(*args, **kwargs):
+        key = str(args) + str(kwargs)
+        if key not in values:
+            values[key] = f(*args, **kwargs)
+        return values[key]
+
+    return wrapper
