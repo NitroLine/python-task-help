e2c14e7acdaa7c4a2f52247f9bf986e5968a096a
AlexMoshkov <moshkov_2002@list.ru>
2021-06-12 14:51:34 +0500

    add 11

---
+++ 11-20/11.py
@@ -0,0 +1,15 @@
+def symbols_count(string):
+	string = string.lower()
+	d = dict()
+	for symbol in string:
+		if symbol not in d:
+			d[symbol] = 0
+		d[symbol] += 1
+	d = sorted(d.items(), key=lambda x: x[1], reverse=True)
+	return d	
+
+
+if __name__ == '__main__':
+	test_string = 'AbraCadaBra'
+	print(symbols_count(test_string))
+	
