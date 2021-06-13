30b9dcc9c7baaac2d76624dda8991da438cbd7f1
NitroLine <48989608+NitroLine@users.noreply.github.com>
2021-06-13 20:43:31 +0500

    rle todo

--- 21-30/30_rle.py
+++ 21-30/30_rle.py
@@ -1,22 +1,44 @@
 # Реализовать RLE-упаковщик и распаковщик. Первое принимает итератор и выдает упакованный итератор,
 # второе принимает упакованный итератор, выдает распакованный. (30 баллов)
 
+
+# TODO fix this
 import itertools
 
 
-def rle_compress(text):
-    for char, same in itertools.groupby(text):
-        count = sum(1 for _ in same) # number of repetitions
-        yield char if count == 1 else str(count)+char
+def rle_encode(data):
+    prev_char = ''
+    count = 1
+    for char in data:
+        if char != prev_char:
+            if prev_char:
+                yield str(count) + prev_char
+            count = 1
+            prev_char = char
+        else:
+            count += 1
+    else:
+        yield str(count) + prev_char
+
+
+def rle_decode(data):
+    count = ''
+    for char in data:
+        if char.isdigit():
+            count += char
+        else:
+            for i in range(len(count)):
+                yield char
+            count = ''
 
 
 def rle_encode(text):
-    text += '\0' # dummy
+    text += '\0'  # dummy
     last = text[0]
     count = 1
     for char in text[1:]:
         if char != last:
-            yield last if count == 1 else str(count)+last
+            yield last if count == 1 else str(count) + last
             last = char
             count = 0
         count += 1
@@ -27,12 +49,12 @@ import unittest
 
 class RLETests(unittest.TestCase):
     def test_edges(self):
-        self.assertEqual(''.join(rle_encode('')), '') # empty
-        self.assertEqual(''.join(rle_encode('a')), 'a') # one
-        self.assertEqual(''.join(rle_encode('aa')), '2a') # two
-        self.assertEqual(''.join(rle_encode('aab')), '2ab') # two + one
-        self.assertEqual(''.join(rle_encode('abb')), 'a2b') # one + two
-        self.assertEqual(''.join(rle_encode('aabb')), '2a2b') # two + two
+        self.assertEqual(''.join(rle_encode('')), '')  # empty
+        self.assertEqual(''.join(rle_encode('a')), 'a')  # one
+        self.assertEqual(''.join(rle_encode('aa')), '2a')  # two
+        self.assertEqual(''.join(rle_encode('aab')), '2ab')  # two + one
+        self.assertEqual(''.join(rle_encode('abb')), 'a2b')  # one + two
+        self.assertEqual(''.join(rle_encode('aabb')), '2a2b')  # two + two
 
     def test_nonletter(self):
         self.assertEqual(''.join(rle_encode('ab b')), 'ab b')
@@ -41,5 +63,6 @@ class RLETests(unittest.TestCase):
     def test_input(self):
         self.assertEqual(''.join(rle_encode('aaabccccCCaB')), '3ab4c2CaB')
 
-    def test_compress(self):
-        self.assertEqual(''.join(rle_compress('aaabccccCCaB')), '3ab4c2CaB')
+    def test_decode(self):
+        self.assertEqual(''.join(rle_decode(''.join(rle_encode('aaabccccCCaB')))),
+                         'aaabccccCCaB')
