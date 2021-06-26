da39a7ec9b2ab0c63a8c20c136c36e722d54d228
NitroLine <48989608+NitroLine@users.noreply.github.com>
2021-06-26 09:50:47 +0500

    add return to 44

--- 41-43/44_moda.py
+++ 41-43/44_moda.py
@@ -10,11 +10,25 @@ def mode(l):
     for item in l:
         if isinstance(item, collections.Hashable):
             if item in d:
-                d[item] += 1
+                d[item][0] += 1
             else:
-                d[item] = 1
+                d[item] = [1, item]
         else:
             if str(item) in d:
-                d[str(item)] += 1
+                d[str(item)][0] += 1
             else:
-                d[str(item)] = 1
+                d[str(item)] = [1, item]
+    ans = []
+    ma = None
+    for it in sorted(d.items(), key=lambda x: x[0], reverse=True):
+        if ma is None:
+            ma = it[0]
+            ans.append(it[1])
+        elif ma == it[0]:
+            ans.append(it[1])
+        else:
+            break
+    return ans
+
+
+
